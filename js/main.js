8c4f2449154fae3e8e9cd2ed165019098d9c00cc
dima92 <dima-tkachenko@mail.ru>
2024-08-21 23:06:04 +0300

    finish task

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const swiperTop = new Swiper('.top__swiper', {
+  effect: 'fade',
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+const swiperAbout = new Swiper(".about__slider", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  freeMode: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
+document.querySelectorAll('.accordeon__triger').forEach((item) => {
+  item.addEventListener('click', () => {
+    item.parentNode.classList.toggle('accordeon__item--active')
+  })
+})
